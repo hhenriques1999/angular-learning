0f45b1b7c4449abb3ca71352f268935b1211e487
hhenriques1999 <hhenriques1999@gmail.com>
2023-07-23 20:55:18 -0300

    Criar interfaces

---
+++ hotelinventoryapp/src/app/rooms/rooms.ts
@@ -0,0 +1,15 @@
+export interface RoomInfo {
+    totalRooms: number;
+    availableRooms: number;
+    bookedRooms: number;
+}
+
+export interface Room {
+    roomNumber: number;
+    roomType: string;
+    amenities: string;
+    price: number;
+    photos: string;
+    checkinTime: Date;
+    checkoutTime: Date;
+}
